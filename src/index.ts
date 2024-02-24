f3493d02ff34bd86aa9935cf539db1fca2d5ac1b
PhongNT19 <phongnt.pc19@gmail.com>
2024-02-24 23:10:52 +0700

    create default folder

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express, { NextFunction, Request, Response } from 'express'
 import databaseService from '~/services/database.services'
 import usersRouter from '~/routes/users.routes'
+import mediasRouter from '~/routes/medias.routes'
 import { defaultErrorHandler } from '~/middlewares/error.middlewares'
+import { initFolder } from '~/utils/file'
 
 databaseService.connect()
 const app = express()
 const port = process.env.PORT
+initFolder()
 app.use(express.json())
+
 app.use('/users', usersRouter)
+app.use('/medias', mediasRouter)
 app.use(defaultErrorHandler)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
